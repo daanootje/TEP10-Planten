d178fc3d1db3a5c6593211c5be9d6474decf3bae
Daan Rotman <daan.rotman@ing.nl>
2017-09-15 15:41:45 +0200

    Initial commit

---
+++ src/main/java/com/tep10/Tep10PlantenApplication.java
@@ -0,0 +1,12 @@
+package com.tep10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tep10PlantenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tep10PlantenApplication.class, args);
+	}
+}
