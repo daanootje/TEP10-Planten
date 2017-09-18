547a110c36e3b6ceb7b6daada9fb1061b1ebc367
Daan Rotman <daan.rotman@ing.nl>
2017-09-18 14:26:02 +0200

    Working MySQL database connection

--- src/main/java/com/tep10/doa/LeverancierJPA.java
+++ src/main/java/com/tep10/doa/LeverancierJPA.java
@@ -5,13 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 
+import java.util.List;
+
 /**
  * Created by UG34QP on 18-9-2017.
  */
 @RepositoryRestController
 public interface LeverancierJPA extends JpaRepository<Leverancier, Long> {
 
-    @Query("select l from Leverancier l where l.levCode = ?1")
-    Leverancier findLeverancierByLevCode(String levCode);
+//    @Query("SELECT l from Leverancier l where l.levcode > 0")
+    Leverancier findLeverancierByLevcode(Long levcode);
+
+    @Query("SELECT l from Leverancier l where l.levcode > 0")
+    List<Leverancier> findByLevcode();
 
 }
--- src/main/java/com/tep10/model/Leverancier.java
+++ src/main/java/com/tep10/model/Leverancier.java
@@ -7,22 +7,27 @@ import lombok.Data;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 /**
  * Created by UG34QP on 18-9-2017.
  */
 
 @Entity
+@Table(name = "leveranciers")
 @Builder
 @AllArgsConstructor
 @Data
 public class Leverancier {
     @Id
-    private Long levCode;
+    private Long levcode;
     @Column
-    private String levNaam;
+    private String levnaam;
     @Column
     private String adres;
     @Column
     private String woonplaats;
+
+    public Leverancier () {
+    }
 }
