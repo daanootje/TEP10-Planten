55e1ad726e915eca777891f4a698d58f9fd2a928
Daan Rotman <daan.rotman@ing.nl>
2017-09-18 11:51:33 +0200

    Inserted working endpoint

--- src/main/java/com/tep10/Tep10PlantenApplication.java
+++ src/main/java/com/tep10/Tep10PlantenApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Tep10PlantenApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Tep10PlantenApplication.class, args);
 	}
